9285d2262d10420a955043c5aa8e56d667abdec2
pragati98891 <pragatisingh12598@gmail.com>
2022-09-15 01:25:16 +0530

    Introduction to JS(variables) problems

---
+++ Day-1-Problem/problem1.js
@@ -0,0 +1,14 @@
+// Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+
+let A = "Masai school";
+let B = "A Transformation in Education";
+console.log(A);
+console.log(B);
+
+
+
+// let x = 12;
+// let y = "12";
+// console.log(x+y);
---
+++ Day-1-Problem/problem4.js
@@ -0,0 +1,32 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let a = "REPORT CARD";
+console.log(a);
+
+let name = "Name - Srijan Singh";
+console.log(name);
+
+let school = "School - St. John's School";
+console.log(school);
+
+let grade = "Grade - 10th Standard";
+console.log(grade);
+
+let section = "Section - D";
+console.log(section);
+
+let rollnumber = "Roll Number - 44";
+console.log(rollnumber);
+
+let subject1 = "Marks Obtain in Mathematics - 83/100";
+console.log(subject1);
+
+let subject2 = "Marks Obtain in Physics - 78/100";
+console.log(subject2);
+
+let subject3 = "Marks Obtain in Chemistry - 66/100";
+console.log(subject3);
